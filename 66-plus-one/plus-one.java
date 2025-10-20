3c2d20886f4d4aad4b038ae90a447126929ffadf
Velaga Sai Manohar <114000710+SaiManohar-007@users.noreply.github.com>
2025-10-20 20:18:31 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (50.30%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -20,16 +20,17 @@ class Solution {
         }
         else{
             for(int i=digits.length-1;i>=0;i--){
-                if(digits[i]==9){
-                    conNine++;
+                if(digits[i]!=9){
+                    digits[i]++;
+                    return digits;
                 }
-                else break;
+                digits[i]=0;
             }
         }
-        for(int i=digits.length-1;i>=digits.length-conNine;i--){
-            digits[i]=0;
-        }
-        digits[digits.length-conNine-1]++;
+        // for(int i=digits.length-1;i>=digits.length-conNine;i--){
+        //     digits[i]=0;
+        // }
+        // digits[digits.length-conNine-1]++;
        
         return digits;
     }
