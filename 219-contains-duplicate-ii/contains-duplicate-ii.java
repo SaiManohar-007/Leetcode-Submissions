d0e63f4fca517ce73f422c5b93e0a54c12891661
Velaga Sai Manohar <114000710+SaiManohar-007@users.noreply.github.com>
2025-10-20 20:47:04 +0530

    Time: 21 ms (79.71%) | Memory: 60.1 MB (28.13%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,18 +3,10 @@ class Solution {
         Map<Integer,Integer> m = new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int ele=nums[i];
-            if(m.containsKey(ele)){
-                int firstOccurence=m.get(ele);
-                if(i-firstOccurence<=k){
-                    return true;
-                }
-                else{
-                    m.put(ele,i);
-                }
+            if(m.containsKey(ele) && i-m.get(ele)<=k){
+                return true;
             }
-            else{
-                m.put(ele,i);
-            }
+            m.put(ele,i);    
         }
         return false;
     }
