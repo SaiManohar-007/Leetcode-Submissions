e4059cd36143b10f8190949ffc8ad0b7a857059b
Velaga Sai Manohar <114000710+SaiManohar-007@users.noreply.github.com>
2025-10-20 20:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (85.17%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,37 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        if(digits[digits.length-1]!=9){
-            digits[digits.length-1]++;
-            return digits;
-        }
-        boolean all9=true;
-        for(int i=0;i<digits.length;i++){
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
             if(digits[i]!=9){
-                all9=false;
-                break;
+                digits[i]++;
+                return digits;
             }
+            digits[i]=0;
         }
-        int[] arr=new int[digits.length+1];
-        int conNine=0;
-        if(all9){
-            arr[0]=1;
-            return arr;
-        }
-        else{
-            for(int i=digits.length-1;i>=0;i--){
-                if(digits[i]!=9){
-                    digits[i]++;
-                    return digits;
-                }
-                digits[i]=0;
-            }
-        }
-        // for(int i=digits.length-1;i>=digits.length-conNine;i--){
-        //     digits[i]=0;
-        // }
-        // digits[digits.length-conNine-1]++;
-       
-        return digits;
+        int[] answer=new int[n+1];
+        answer[0]=1;
+        return answer;
     }
 }
