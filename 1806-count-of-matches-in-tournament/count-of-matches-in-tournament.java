00ef2beda0a8446ebd3448442d94e9e6634780ec
Velaga Sai Manohar <114000710+SaiManohar-007@users.noreply.github.com>
2025-10-25 11:30:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (5.25%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int m=0;
+        while(n!=1){
+            int rem=n%2;
+            m+=(n-rem)/2;
+            m+=rem;
+            n/=2;
+        }
+        return m;
+        
+    }
+}
